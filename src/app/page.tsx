8e796cfc7170b2758ff01fe434d640755857aefa
Chiragkumar Gohil <chiragkumargohil@outlook.com>
2024-07-05 01:31:19 +0530

    content change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Dashboard() {
       <div className="flex-col md:flex">
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Hey, Chirag!</h2>
+            <h2 className="text-3xl font-bold tracking-tight">Namaste!</h2>
             <div className="flex items-center gap-4">
               <Button asChild>
                 <Link
