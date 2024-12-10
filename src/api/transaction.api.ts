2329077d6b05cffd34f37ec9fa8dd2350f4fc2a4
Chiragkumar Gohil <chiragkumargohil@outlook.com>
2024-12-10 21:26:33 +0530

    transaction trend bug

--- src/api/transaction.api.ts
+++ src/api/transaction.api.ts
@@ -377,7 +377,7 @@ async function getOverview(email: string) {
     });
 
     responses[4] = new Array(12).fill(0).map((_, index) => {
-      const month = index;
+      const month = index + 1;
       const data = responses[4].find((item: any) => item.month == month);
       return {
         name: getMonthNameFromNumber(month),
